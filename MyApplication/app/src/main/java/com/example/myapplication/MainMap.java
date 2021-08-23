1396af42759a42a036804a3592668baddb42131f
nimajneben <47288387+nimajneben@users.noreply.github.com>
2021-08-22 23:08:51 -0400

    clean up imports

--- MyApplication/app/src/main/java/com/example/myapplication/MainMap.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainMap.java
@@ -12,21 +12,13 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 //import androidx.fragment.app.FragmentActivity;
 
-//import android.os.Bundle;
 
-/*
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.test.databinding.ActivityMapsBinding;
 
     }
 
